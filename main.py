15df9f61df770ba45f5489893470fe59a1df0e2a
vitalii <it-finally@pm.me>
2025-01-02 20:11:13 +0100

    added screen render and gameloop

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ import pygame
 
 def main():
     print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill('black')
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
