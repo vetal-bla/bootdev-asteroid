72fa26e2a34658f7257c6dfde4cd7f7d54c5f12f
vitalii <it-finally@pm.me>
2025-01-02 21:16:51 +0100

    added containers for objects

--- main.py
+++ main.py
@@ -10,22 +10,32 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    # groups for classes
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    # added player to both group of update and draw
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        # move player
-        player.update(dt)
-        
+
+        # move objects
+        for obj in updatable:
+            obj.update(dt)
+
         # add black background
         screen.fill('black')
 
-        # render player
-        player.draw(screen)
+        # render objects
+        for obj in drawable:
+            obj.draw(screen)
 
         # update screen
         pygame.display.flip()
